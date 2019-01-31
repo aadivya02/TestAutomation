7f4702767923822d34bb1df41e94e417fcf3258d
Aadivya Prakash <aadivyaprakash@gmail.com>
2019-01-31 20:31:57 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        //System.out.println("BootCamp");
+        String s = "BootBo camp";
+        System.out.println(s.replace('B','c'));
+       // System.out.println(s.replaceFirst('Bo','ca'));
+
+
+
+
+    }
+}
